2e03576d80b0d696f767d525c4c4eaf0e442ecdf
Milroy Travasso <milroytravasso@hotmail.com>
2018-12-05 00:05:36 +0000

    suggested changes

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,4 @@
-const accessToken = '';
+let accessToken = '';
 const clientID = '48a9d52bbb8148e88009c595ce498aa1';
 const redirectURI = 'http://localhost:3000/';
 
@@ -9,10 +9,12 @@ const Spotify = {
       return accessToken;
     } else if(window.location.href.match(/access_token=([^&]*)/) && window.location.href.match(/expires_in=([^&]*)/)) {
       console.log('Access token extracted from URI');
-      accessToken = window.location.href.match(/access_token=([^&]*)/);
-      const expiresIn = window.location.href.match(/expires_in=([^&]*)/);
+      const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+      accessToken = accessTokenMatch[1];
+      let expiresIn = window.location.href.match(/expires_in=([^&]*)/);
       window.setTimeout(() => accessToken = '', expiresIn * 1000);
       window.history.pushState('Access Token', null, '/');
+      return accessToken;
     } else {
       console.log('Trying final option to access token');
       window.location=`https://accounts.spotify.com/authorize?client_id=${clientID}}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
